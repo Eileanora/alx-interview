4ac600e5ec3dd4be413efeeac19a723e5db8621c
yasmeen <yasmeenhany14@gmail.com>
2024-01-04 17:02:52 +0200

    task done

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,17 @@
+'''Module defining pascal traingle function'''
+
+
+def pascal_traingle(n):
+    '''Function to create pascale traingle of size n'''
+    pascalTraingle = []
+    for row in range(0, n):
+        pascalTraingle.append([])
+        for col in range(0, row + 1):
+            if col == 0 or col == row:
+                pascalTraingle[row].append(1)
+            else:
+                pascalTraingle[row].append(
+                    pascalTraingle[row - 1][col]
+                    + pascalTraingle[row - 1][col - 1])
+
+    return pascalTraingle
