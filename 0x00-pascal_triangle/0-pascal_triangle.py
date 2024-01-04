d9f7eb6a1362e010e490abcde4f596221b6f1ac1
yasmeen <yasmeenhany14@gmail.com>
2024-01-04 18:23:06 +0200

    fixed function name

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,19 +2,19 @@
 '''Module defining pascal traingle function'''
 
 
-def pascal_traingle(n):
+def pascal_triangle(n):
     '''Function to create pascale traingle of size n'''
     if n <= 0:
         return []
-    traingle = []
+    triangle = []
     for row in range(0, n):
-        traingle.append([])
+        triangle.append([])
         for col in range(0, row + 1):
             if col == 0 or col == row:
-                traingle[row].append(1)
+                triangle[row].append(1)
             else:
-                traingle[row].append(
-                    traingle[row - 1][col]
-                    + traingle[row - 1][col - 1])
+                triangle[row].append(
+                    triangle[row - 1][col]
+                    + triangle[row - 1][col - 1])
 
-    return traingle
+    return triangle
