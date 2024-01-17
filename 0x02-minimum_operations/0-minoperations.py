f9f02275c9f3d8e7548e8988cad965662ee898f7
yasmeen <yasmeenhany14@gmail.com>
2024-01-17 21:03:06 +0200

    consider n being lower than 1

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,7 +6,7 @@ def minOperations(n):
     '''Given a number n, returns the fewest number of operations needed to \
     result in exactly n H characters in the file.'''
     def helper(n):
-        if n == 1:
+        if n <= 1:
             return 0
 
         for i in range(2, (n // 2) + 1):
