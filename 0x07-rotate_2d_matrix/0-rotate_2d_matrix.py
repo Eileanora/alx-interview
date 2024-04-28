984135de26505b6278170a9f2bcc36379853bc01
yasmeen <yasmeenhany14@gmail.com>
2024-04-29 02:45:55 +0300

    fix1

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 ''' Module for the rotate_2d_matrix method '''
-from typing import List
 
 
-def rotate_2d_matrix(matrix: List[List[int]]) -> None:
+def rotate_2d_matrix(matrix):
     ''' Rotates a 2D matrix 90 degrees clockwise '''
+    if type(matrix) is not list or matrix == []:
+        return None
+
+    if not all(isinstance(row, list) for row in matrix):
+        return None
+
     n = len(matrix)
     for x in range(n // 2):
         offset = 0
@@ -15,4 +20,3 @@ def rotate_2d_matrix(matrix: List[List[int]]) -> None:
             matrix[n - 1 - x][n - 1 - y] = matrix[y][n - 1 - x]
             matrix[y][n - 1 - x] = top
             offset += 1
-    return None
