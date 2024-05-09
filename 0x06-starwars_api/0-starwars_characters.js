5018b6b5ec9f70bcaccc5a2d056969958c81aad4
yasmeen <yasmeenhany14@gmail.com>
2024-05-10 01:42:43 +0300

    fix semistanderd

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const request = require('request');
-const util = require('util')
+const util = require('util');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 /// pass cmd line arg to url which is the movie id
 
 const requestPromise = util.promisify(request);
-async function getCharacterName(character) {
+async function getCharacterName (character) {
   const response = await requestPromise(character);
   if (response.statusCode === 200) {
     const data = JSON.parse(response.body);
